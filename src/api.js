69ffeb7af6258cf359b6891862a03e13cb0034da
testtestae <a123@MacBook-Pro-123.local>
2020-01-13 23:37:02 +0500

    create clean structure components

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import openSocket from 'socket.io-client';
+
 const  socket = openSocket('http://localhost:8000');
+
 function subscribeToTimer(cb) {
     socket.on('timer', timestamp => cb(null, timestamp));
     socket.emit('subscribeToTimer', 1000);
 }
-export { subscribeToTimer };
+
+export default subscribeToTimer;
